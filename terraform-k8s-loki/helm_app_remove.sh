97963ba91b44692d40b5e46bfd03be34a52b09da
Anton Patsev <patsev.anton@gmail.com>
2022-11-04 11:24:07 +0600

    serviceMonitor true promtail

--- terraform-k8s-loki/helm_app_remove.sh
+++ terraform-k8s-loki/helm_app_remove.sh
@@ -5,7 +5,9 @@ set -eu pipefail
 # uninstall Kube-Prometheus-Stack
 echo ""
 echo "helm uninstall Kube-Prometheus-Stack"
+helm uninstall loki || true
 helm uninstall promgrafana || true
+helm uninstall promtail || true
 kubectl delete crd alertmanagerconfigs.monitoring.coreos.com || true
 kubectl delete crd alertmanagers.monitoring.coreos.com || true
 kubectl delete crd podmonitors.monitoring.coreos.com || true
