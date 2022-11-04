fc93c7be649658be8df761909e9181ca58d1f6a9
Anton Patsev <patsev.anton@gmail.com>
2022-11-04 09:49:12 +0600

    add helm install Loki, Promtail to terraform-k8s-loki

--- terraform-k8s-loki/helm_app_install.sh
+++ terraform-k8s-loki/helm_app_install.sh
@@ -11,9 +11,20 @@ echo "Install Kube-Prometheus-Stack"
 helm repo add prometheus-community https://prometheus-community.github.io/helm-charts
 helm repo update
 helm upgrade --install promgrafana prometheus-community/kube-prometheus-stack -f values-kube-prometheus-stack.yaml
-#echo ""
-#echo "Print jenkins password"
-#kubectl exec --namespace default -it svc/jenkins -c jenkins -- /bin/cat /run/secrets/additional/chart-admin-password && echo
+
+# Install Microservices deployment Loki
+echo ""
+echo "Install Microservices deployment Loki"
+helm repo add grafana https://grafana.github.io/helm-charts
+helm repo update
+helm upgrade --install loki grafana/loki-distributed
+
+# Install Promtail
+echo ""
+echo "Install Promtail"
+helm repo add grafana https://grafana.github.io/helm-charts
+helm repo update
+helm upgrade --install promtail grafana/promtail --set "loki.serviceName=loki"
 
 end_time=`date +%s`
 date2=$(date +"%s")
