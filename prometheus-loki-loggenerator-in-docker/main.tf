a2edc85fca71cda87a229934c4b2db602f280385
Anton Patsev <patsev.anton@gmail.com>
2022-11-10 15:58:41 +0600

    prometheus-loki-loggenerator-in-docker

---
+++ prometheus-loki-loggenerator-in-docker/main.tf
@@ -0,0 +1,59 @@
+module "javaindocker" {
+  source             = "../terraform-yandex-compute"
+  image_family       = var.family_images_linux
+  subnet_id          = yandex_vpc_subnet.subnet-1.id
+  zone               = var.yc_zone
+  name               = "javaindocker"
+  hostname           = "javaindocker"
+  size               = 30
+  is_nat             = true
+  preemptible        = true
+  core_fraction      = 50
+  service_account_id = yandex_iam_service_account.sa-compute-admin.id
+  depends_on = [
+    yandex_vpc_subnet.subnet-1,
+    yandex_iam_service_account.sa-compute-admin
+  ]
+}
+
+module "prometheus" {
+  source             = "../terraform-yandex-compute"
+  image_family       = var.family_images_linux
+  subnet_id          = yandex_vpc_subnet.subnet-1.id
+  zone               = var.yc_zone
+  name               = "prometheus"
+  hostname           = "prometheus"
+  is_nat             = true
+  preemptible        = true
+  core_fraction      = 50
+  service_account_id = yandex_iam_service_account.sa-compute-admin.id
+  depends_on = [
+    yandex_vpc_subnet.subnet-1,
+    yandex_iam_service_account.sa-compute-admin
+  ]
+}
+
+resource "yandex_vpc_network" "network-1" {
+  name = "network1"
+}
+
+resource "yandex_vpc_subnet" "subnet-1" {
+  name           = "subnet1"
+  zone           = "ru-central1-b"
+  network_id     = yandex_vpc_network.network-1.id
+  v4_cidr_blocks = ["192.168.10.0/24"]
+}
+
+resource "local_file" "inventory_yml" {
+  content = templatefile("inventory_yml.tpl",
+    {
+      ssh_user               = var.ssh_user
+      hostname_prometheus    = "prometheus"
+      hostname_javaindocker  = "javaindocker"
+      public_ip_prometheus   = module.prometheus.external_ip[0]
+      public_ip_javaindocker = module.javaindocker.external_ip[0]
+      domain                 = var.domain
+    }
+  )
+  filename = "inventory.yml"
+}
