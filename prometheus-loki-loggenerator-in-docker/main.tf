86ff6fb1fcd306fa6441ab4822059708df036be2
Anton Patsev <patsev.anton@gmail.com>
2022-11-10 19:16:02 +0600

    data.yandex_vpc_subnet.default-ru-central1-a.id prometheus-loki-loggenerator-in-docker

--- prometheus-loki-loggenerator-in-docker/main.tf
+++ prometheus-loki-loggenerator-in-docker/main.tf
@@ -1,7 +1,7 @@
 module "javaindocker" {
   source             = "../terraform-yandex-compute"
   image_family       = var.family_images_linux
-  subnet_id          = yandex_vpc_subnet.subnet-1.id
+  subnet_id          = data.yandex_vpc_subnet.default-ru-central1-a.id
   zone               = var.yc_zone
   name               = "javaindocker"
   hostname           = "javaindocker"
@@ -11,7 +11,6 @@ module "javaindocker" {
   core_fraction      = 50
   service_account_id = yandex_iam_service_account.sa-compute-admin.id
   depends_on = [
-    yandex_vpc_subnet.subnet-1,
     yandex_iam_service_account.sa-compute-admin
   ]
 }
@@ -19,7 +18,7 @@ module "javaindocker" {
 module "prometheus" {
   source             = "../terraform-yandex-compute"
   image_family       = var.family_images_linux
-  subnet_id          = yandex_vpc_subnet.subnet-1.id
+  subnet_id          = data.yandex_vpc_subnet.default-ru-central1-a.id
   zone               = var.yc_zone
   name               = "prometheus"
   hostname           = "prometheus"
@@ -28,20 +27,8 @@ module "prometheus" {
   core_fraction      = 50
   service_account_id = yandex_iam_service_account.sa-compute-admin.id
   depends_on = [
-    yandex_vpc_subnet.subnet-1,
     yandex_iam_service_account.sa-compute-admin
   ]
-}
-
-resource "yandex_vpc_network" "network-1" {
-  name = "network1"
-}
-
-resource "yandex_vpc_subnet" "subnet-1" {
-  name           = "subnet1"
-  zone           = "ru-central1-b"
-  network_id     = yandex_vpc_network.network-1.id
-  v4_cidr_blocks = ["192.168.10.0/24"]
 }
 
 resource "local_file" "inventory_yml" {
