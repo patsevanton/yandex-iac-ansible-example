bf63ccc4dcc1db001c1c63f7f77835b15d3b549d
Anton Patsev <patsev.anton@gmail.com>
2022-11-13 19:00:03 +0600

    0.81.0 provider yandex

--- terraform-k8s-opensearch/provider.tf
+++ terraform-k8s-opensearch/provider.tf
@@ -4,7 +4,7 @@ terraform {
   required_providers {
     yandex = {
       source  = "yandex-cloud/yandex"
-      version = "0.82.0"
+      version = "0.81.0"
     }
   }
 }
--- vault_docker/provider.tf
+++ vault_docker/provider.tf
@@ -2,7 +2,7 @@ terraform {
   required_providers {
     yandex = {
       source  = "yandex-cloud/yandex"
-      version = "0.82.0"
+      version = "0.81.0"
     }
     template = {
       source  = "terraform-registry.storage.yandexcloud.net/hashicorp/template"
