34fcb29d7cb090453e9af1d8f5108c99a6c54e61
Anton Patsev <patsev.anton@gmail.com>
2022-11-14 13:01:20 +0600

    remove second vm in prometheus-loki-loggenerator-in-docker

--- prometheus-loki-loggenerator-in-docker/main.tf
+++ prometheus-loki-loggenerator-in-docker/main.tf
@@ -15,29 +15,11 @@ module "lokiindocker" {
   ]
 }
 
-module "prometheus" {
-  source             = "../terraform-yandex-compute"
-  image_family       = var.family_images_linux
-  subnet_id          = data.yandex_vpc_subnet.default-ru-central1-a.id
-  zone               = data.yandex_vpc_subnet.default-ru-central1-a.zone
-  name               = "prometheus"
-  hostname           = "prometheus"
-  is_nat             = true
-  preemptible        = true
-  core_fraction      = 50
-  service_account_id = yandex_iam_service_account.sa-compute-admin.id
-  depends_on = [
-    yandex_iam_service_account.sa-compute-admin
-  ]
-}
-
 resource "local_file" "inventory_yml" {
   content = templatefile("inventory_yml.tpl",
     {
       ssh_user               = var.ssh_user
-      hostname_prometheus    = "prometheus"
       hostname_lokiindocker  = "lokiindocker"
-      public_ip_prometheus   = module.prometheus.external_ip[0]
       public_ip_lokiindocker = module.lokiindocker.external_ip[0]
     }
   )
