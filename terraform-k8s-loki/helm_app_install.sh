9e114d7a7c3b110832918ab663869bd18ab3c755
Anton Patsev <patsev.anton@gmail.com>
2022-11-20 20:55:29 +0600

    terraform loki Set grafana datasource loki

--- terraform-k8s-loki/helm_app_install.sh
+++ terraform-k8s-loki/helm_app_install.sh
@@ -14,8 +14,8 @@ echo ""
 echo "Install Kube-Prometheus-Stack"
 helm repo add prometheus-community https://prometheus-community.github.io/helm-charts
 helm repo update
-werf helm upgrade --install --wait kube-prometheus-stack prometheus-community/kube-prometheus-stack \
-     --version 41.8.0 -f values-kube-prometheus-stack.yaml
+werf helm upgrade --install --wait kube-prometheus-stack prometheus-community/kube-prometheus-stack --version 41.8.0
+#    -f values-kube-prometheus-stack.yaml
 
 
 #echo ""
@@ -25,15 +25,16 @@ werf helm upgrade --install --wait kube-prometheus-stack prometheus-community/ku
 #kubectl create namespace cassandra || true
 #werf helm upgrade --install --wait cassandra bitnami/cassandra -n cassandra -f value-cassandra.yaml
 
-
-#echo ""
-#echo "Install Microservices deployment Loki"
-#helm repo add grafana https://grafana.github.io/helm-charts
-#helm repo update
-#kubectl create namespace loki || true
+# Set grafana datasource loki
+echo "http://loki-loki-distributed-gateway.loki"
+echo ""
+echo "Install Microservices deployment Loki"
+helm repo add grafana https://grafana.github.io/helm-charts
+helm repo update
+kubectl create namespace loki || true
 #export access_key_id=$(terraform output --raw access_key_sa_storage_admin_for_test_bucket)
 #export secret_access_key=$(terraform output --raw secret_key_sa_storage_admin_for_test_bucket)
-#werf helm upgrade --install --wait loki grafana/loki-distributed -n loki
+werf helm upgrade --install --wait loki grafana/loki-distributed -n loki --version 0.65.1
 
 #    --set "loki.storageConfig.aws.access_key_id=$access_key_id"  \
 #    --set "loki.storageConfig.aws.secret_access_key=$secret_access_key"  \
@@ -41,17 +42,17 @@ werf helm upgrade --install --wait kube-prometheus-stack prometheus-community/ku
 #    ../../grafana-helm-charts/charts/loki-distributed
 
 
-#echo ""
-#echo "Install Promtail"
-#helm repo add grafana https://grafana.github.io/helm-charts
-#helm repo update
-#kubectl create namespace promtail || true
-#werf helm upgrade --install --wait promtail grafana/promtail -n promtail --set "loki.serviceName=loki" -f values-promtail.yaml
-#
-#echo ""
-#echo "Install loggenerator"
-#kubectl create namespace loggenerator || true
-#werf helm upgrade --install --wait loggenerator -n loggenerator ./loggenerator
+echo ""
+echo "Install Promtail"
+helm repo add grafana https://grafana.github.io/helm-charts
+helm repo update
+kubectl create namespace promtail || true
+werf helm upgrade --install --wait promtail grafana/promtail -n promtail --set "loki.serviceName=loki" --version 6.6.2 -f values-promtail.yaml
+
+echo ""
+echo "Install loggenerator"
+kubectl create namespace loggenerator || true
+werf helm upgrade --install --wait loggenerator -n loggenerator ./loggenerator
 
 end_time=`date +%s`
 date2=$(date +"%s")
