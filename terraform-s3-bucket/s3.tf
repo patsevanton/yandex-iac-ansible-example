a6e9dfee31740e46c129b43a121535d0713f89cf
Anton Patsev <patsev.anton@gmail.com>
2022-12-05 11:12:57 +0600

    terraform-s3-bucket velero-sa-static-key

--- terraform-s3-bucket/s3.tf
+++ terraform-s3-bucket/s3.tf
@@ -1,31 +1,31 @@
 ## Create Static Access Keys
-resource "yandex_iam_service_account_static_access_key" "sa-storage-admin-static-key" {
-  service_account_id = yandex_iam_service_account.sa-storage-admin.id
+resource "yandex_iam_service_account_static_access_key" "velero-sa-static-key" {
+  service_account_id = yandex_iam_service_account.velero-sa.id
   description        = "static access key for object storage"
 }
 
 ## Use keys to create bucket
-resource "yandex_storage_bucket" "loki" {
-  access_key = yandex_iam_service_account_static_access_key.sa-storage-admin-static-key.access_key
-  secret_key = yandex_iam_service_account_static_access_key.sa-storage-admin-static-key.secret_key
-  bucket     = "test-backet-anton-patsev"
+resource "yandex_storage_bucket" "velero" {
+  access_key = yandex_iam_service_account_static_access_key.velero-sa-static-key.access_key
+  secret_key = yandex_iam_service_account_static_access_key.velero-sa-static-key.secret_key
+  bucket     = "velero-backet-anton-patsev"
   force_destroy = true
 }
 
 output "yandex_storage_bucket_loki_access_key" {
-  description = "access_key yandex_storage_bucket of loki"
-  value       = yandex_storage_bucket.loki.access_key
+  description = "access_key yandex_storage_bucket of velero"
+  value       = yandex_storage_bucket.velero.access_key
   sensitive   = true
 }
 
 output "yandex_storage_bucket_loki_secret_key" {
-  description = "secret_key yandex_storage_bucket of loki"
-  value       = yandex_storage_bucket.loki.secret_key
+  description = "secret_key yandex_storage_bucket of velero"
+  value       = yandex_storage_bucket.velero.secret_key
   sensitive   = true
 }
 
 output "yandex_storage_bucket_loki_bucket" {
   description = "name bucket of loki"
-  value       = yandex_storage_bucket.loki.bucket
+  value       = yandex_storage_bucket.velero.bucket
   sensitive   = true
 }
