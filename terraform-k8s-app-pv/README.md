c14d77bad98d4bc4589e31463d7026402f199dca
Anton Patsev <patsev.anton@gmail.com>
2022-12-05 11:45:31 +0600

    terraform-s3-bucket velero install

--- terraform-k8s-app-pv/README.md
+++ terraform-k8s-app-pv/README.md
@@ -1,9 +1,9 @@
-## View logs loki
-```
-stern -n loki .
-```
+### Create large ~1 GB random dataset in PostgreSQL
 
-## View logs promtail
 ```
-stern -n promtail .
+CREATE TABLE large_test (num1 bigint, num2 double precision, num3 double precision);
+
+INSERT INTO large_test (num1, num2, num3)
+  SELECT round(random()*10), random(), random()*142
+  FROM generate_series(1, 20000000) s(i);
 ```
---
+++ terraform-s3-bucket/velero_install.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+set -eu pipefail
+
+export bucket=$(terraform output --raw yandex_storage_bucket_loki_bucket)
+
+kubectl label volumesnapshotclasses.snapshot.storage.k8s.io yc-csi-snapclass \
+velero.io/csi-volumesnapshot-class="true"
+
+
+velero install \
+  --backup-location-config s3Url=https://storage.yandexcloud.net,region=ru-central1 \
+  --bucket $bucket \
+  --plugins velero/velero-plugin-for-aws:v1.3.0,velero/velero-plugin-for-csi:v0.2.0 \
+  --provider aws \
+  --secret-file ./credentials \
+  --features=EnableCSI \
+  --use-volume-snapshots=true \
+  --snapshot-location-config region=ru-central1
