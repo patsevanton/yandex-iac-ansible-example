9002f13e3edf6de66dac8c623e5af685e6131b13
Anton Patsev <patsev.anton@gmail.com>
2022-12-07 09:41:26 +0600

    terraform-k8s-loki disable yandex_dns_zone apatsev_org_ru

--- terraform-k8s-loki/dns.tf
+++ terraform-k8s-loki/dns.tf
@@ -1,13 +1,13 @@
-resource "yandex_dns_zone" "apatsev_org_ru" {
-  name   = "apatsev-org-ru"
-  zone   = "apatsev.org.ru."
-  public = true
-}
-
-resource "yandex_dns_recordset" "loki_apatsev_org_ru" {
-  zone_id = yandex_dns_zone.apatsev_org_ru.id
-  name    = "loki.apatsev.org.ru."
-  type    = "A"
-  ttl     = 200
-  data    = [yandex_vpc_address.loki_address.external_ipv4_address[0].address]
-}
+#resource "yandex_dns_zone" "apatsev_org_ru" {
+#  name   = "apatsev-org-ru"
+#  zone   = "apatsev.org.ru."
+#  public = true
+#}
+#
+#resource "yandex_dns_recordset" "loki_apatsev_org_ru" {
+#  zone_id = yandex_dns_zone.apatsev_org_ru.id
+#  name    = "loki.apatsev.org.ru."
+#  type    = "A"
+#  ttl     = 200
+#  data    = [yandex_vpc_address.loki_address.external_ipv4_address[0].address]
+#}
