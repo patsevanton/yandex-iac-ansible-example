c8f528c0c599aaadee07a009a253cf80d3e85c7e
Anton Patsev <patsev.anton@gmail.com>
2022-12-27 18:40:32 +0600

    change provider to 0.84.0

--- k8s-jenkins-x/provider.tf
+++ k8s-jenkins-x/provider.tf
@@ -21,7 +21,7 @@ terraform {
   required_providers {
     yandex = {
       source  = "yandex-cloud/yandex"
-      version = "0.82.0"
+      version = "0.84.0"
     }
   }
 }
--- sonarqube/provider.tf
+++ sonarqube/provider.tf
@@ -2,7 +2,7 @@ terraform {
   required_providers {
     yandex = {
       source  = "yandex-cloud/yandex"
-      version = "0.82.0"
+      version = "0.84.0"
     }
     local = {
       source  = "terraform-registry.storage.yandexcloud.net/hashicorp/local"
