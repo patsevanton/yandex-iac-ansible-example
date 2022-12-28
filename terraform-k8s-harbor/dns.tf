1d7e5ffdfa12c712c4b0020fc8a655c788254668
Anton Patsev <patsev.anton@gmail.com>
2022-12-28 20:44:45 +0600

    terraform-k8s-harbor values-harbor.yaml

--- terraform-k8s-harbor/dns.tf
+++ terraform-k8s-harbor/dns.tf
@@ -11,3 +11,11 @@ resource "yandex_dns_recordset" "harbor_apatsev_org_ru" {
  ttl     = 200
  data    = [yandex_vpc_address.harbor_address.external_ipv4_address[0].address]
 }
+
+resource "yandex_dns_recordset" "notary_apatsev_org_ru" {
+ zone_id = yandex_dns_zone.apatsev_org_ru.id
+ name    = "notary.apatsev.org.ru."
+ type    = "A"
+ ttl     = 200
+ data    = [yandex_vpc_address.harbor_address.external_ipv4_address[0].address]
+}
--- terraform-k8s-harbor/helm_app_install.sh
+++ terraform-k8s-harbor/helm_app_install.sh
@@ -10,7 +10,8 @@ echo "harbor"
 helm repo add harbor https://helm.goharbor.io
 helm repo update
 kubectl create namespace harbor || true
-helm upgrade --install --wait -n harbor harbor harbor/harbor --set externalURL=https://harbor.apatsev.org.ru --set expose.ingress.hosts.core=harbor.apatsev.org.ru --set harborAdminPassword=Harbor12345 --set expose.tls.secretName=letsencrypt-prod  --set expose.ingress.annotations="cert-manager.io/cluster-issuer=letsencrypt-prod"
+helm upgrade --install --wait -n harbor harbor harbor/harbor -f values-harbor.yaml
+#helm upgrade --install --wait -n harbor harbor harbor/harbor --set externalURL=https://harbor.apatsev.org.ru --set expose.ingress.hosts.core=harbor.apatsev.org.ru --set harborAdminPassword=Harbor12345 --set expose.tls.secretName=letsencrypt-prod  --set expose.ingress.annotations="cert-manager.io/cluster-issuer=letsencrypt-prod"
 
 
 end_time=`date +%s`
