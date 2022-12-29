4a206fe091039cb379605f8e094a2b44a6bf21e8
Anton Patsev <patsev.anton@gmail.com>
2022-12-29 17:22:15 +0600

    terraform-k8s-sentry add postgres

---
+++ terraform-k8s-sentry/postgres.tf
@@ -0,0 +1,41 @@
+resource "yandex_mdb_postgresql_cluster" "test" {
+  name                = "test"
+  environment         = "PRODUCTION"
+  network_id          = "enprkje8ae9b74e0himb"
+
+  config {
+    version = "14"
+    resources {
+      resource_preset_id = "s3-c2-m8"
+      disk_type_id       = "network-ssd"
+      disk_size          = 20
+    }
+    performance_diagnostics {
+      enabled = true
+      sessions_sampling_interval  = 60
+      statements_sampling_interval = 600
+    }
+  }
+
+  host {
+    zone      = "ru-central1-b"
+    name      = "test"
+    subnet_id = "e2l6251f60t5e6faq3o7"
+    assign_public_ip = true
+  }
+}
+
+resource "yandex_mdb_postgresql_database" "test" {
+  cluster_id = yandex_mdb_postgresql_cluster.test.id
+  name       = "test"
+  owner      = "test"
+  depends_on = [
+    yandex_mdb_postgresql_user.test
+  ]
+}
+
+resource "yandex_mdb_postgresql_user" "test" {
+  cluster_id = yandex_mdb_postgresql_cluster.test.id
+  name       = "test"
+  password   = "dsgvdgsbsdznbzdfsndf"
+}
