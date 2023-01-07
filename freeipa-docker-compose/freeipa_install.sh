5651495dfa2c172362d3f17c0cc19d987484b4ac
Anton Patsev <patsev.anton@gmail.com>
2023-01-07 18:32:02 +0600

    freeipa-docker-compose install docker

--- freeipa-docker-compose/freeipa_install.sh
+++ freeipa-docker-compose/freeipa_install.sh
@@ -7,7 +7,6 @@ date1=$(date +"%s")
 TF_IN_AUTOMATION=1 terraform init -upgrade
 TF_IN_AUTOMATION=1 terraform apply -auto-approve
 rm -rf ansible-freeipa || true
-ansible-galaxy collection install freeipa.ansible_freeipa
 ansible-playbook -i inventory.yml playbook.yml
 end_time=`date +%s`
 date2=$(date +"%s")
