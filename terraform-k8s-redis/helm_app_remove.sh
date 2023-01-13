0a4a6d8862200e769a7322988449b443e017bece
Anton Patsev <patsev.anton@gmail.com>
2023-01-13 17:57:21 +0600

    terraform-k8s-app-redis helm uninstall -n kube-prometheus-stack

--- terraform-k8s-redis/helm_app_remove.sh
+++ terraform-k8s-redis/helm_app_remove.sh
@@ -5,19 +5,10 @@ set -eu pipefail
 # uninstall Kube-Prometheus-Stack
 echo ""
 echo "helm uninstall Kube-Prometheus-Stack"
-helm uninstall -n loki loki || true
-helm uninstall loki || true
-helm uninstall kube-prometheus-stack || true
+helm uninstall -n redis redis || true
 helm uninstall -n kube-prometheus-stack kube-prometheus-stack || true
-helm uninstall -n promtail promtail || true
-helm uninstall -n loggenerator loggenerator || true
-helm uninstall -n cassandra cassandra || true
-helm uninstall -n vector vector || true
-kubectl delete namespace loki || true
-kubectl delete namespace promtail || true
-kubectl delete namespace loggenerator || true
-kubectl delete namespace cassandra || true
-kubectl delete namespace vector || true
+kubectl delete namespace redis || true
+kubectl delete namespace kube-prometheus-stack || true
 kubectl delete crd alertmanagerconfigs.monitoring.coreos.com || true
 kubectl delete crd alertmanagers.monitoring.coreos.com || true
 kubectl delete crd podmonitors.monitoring.coreos.com || true
