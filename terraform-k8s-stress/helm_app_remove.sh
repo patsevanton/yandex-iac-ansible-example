2dc62d7e8c2527759f449e8d7e89ceecf81e888b
Anton Patsev <patsev.anton@gmail.com>
2023-01-14 22:19:05 +0600

    terraform-k8s-stress rm -f values-kube-prometheus-stack.yaml

--- terraform-k8s-stress/helm_app_remove.sh
+++ terraform-k8s-stress/helm_app_remove.sh
@@ -7,7 +7,6 @@ echo ""
 echo "helm uninstall Kube-Prometheus-Stack"
 helm uninstall -n stress stress || true
 helm uninstall -n kube-prometheus-stack kube-prometheus-stack || true
-rm -f values-kube-prometheus-stack.yaml
 kubectl delete namespace stress || true
 kubectl delete namespace kube-prometheus-stack || true
 kubectl delete crd alertmanagerconfigs.monitoring.coreos.com || true
--- terraform-k8s-stress/k8s_destroy.sh
+++ terraform-k8s-stress/k8s_destroy.sh
@@ -7,6 +7,7 @@ date1=$(date +"%s")
 TF_IN_AUTOMATION=1 terraform destroy -auto-approve
 rm -f terraform.log
 rm -f /home/$USER/.kube/config
+rm -f values-kube-prometheus-stack.yaml
 end_time=`date +%s`
 date2=$(date +"%s")
 echo "###############"
