2b1a405ae0a27bdacf0b225230b32917e42f4211
Anton Patsev <patsev.anton@gmail.com>
2023-01-16 16:51:15 +0600

    freeipa-docker-compose ansible-playbook -i inventory.yml playbook.yml

--- freeipa-docker-compose/freeipa_install.sh
+++ freeipa-docker-compose/freeipa_install.sh
@@ -7,7 +7,7 @@ date1=$(date +"%s")
 TF_IN_AUTOMATION=1 terraform init -upgrade
 TF_IN_AUTOMATION=1 terraform apply -auto-approve
 ansible-galaxy install geerlingguy.docker
-ansible-playbook -i inventory.yml playbook2.yml
+ansible-playbook -i inventory.yml playbook.yml
 end_time=$(date +%s)
 date2=$(date +"%s")
 echo "###############"
