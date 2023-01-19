59a2dc090185ea6eca0617374471a29aa4a018af
Anton Patsev <patsev.anton@gmail.com>
2023-01-19 07:29:24 +0600

    terraform-coi-before-ingress/service-accounts.tf

--- terraform-coi-before-ingress/container-optimized-image.tf
+++ terraform-coi-before-ingress/container-optimized-image.tf
@@ -2,35 +2,6 @@ data "yandex_compute_image" "container-optimized-image" {
   family = "container-optimized-image"
 }
 
-resource "yandex_iam_service_account" "coi-sa" {
-  folder_id = var.yc_folder_id
-  name      = "coi-sa"
-}
-
-resource "yandex_resourcemanager_folder_iam_member" "coi-compute-admin-permissions" {
-  folder_id = var.yc_folder_id
-  role      = "compute.admin"
-  member    = "serviceAccount:${yandex_iam_service_account.coi-sa.id}"
-}
-
-resource "yandex_resourcemanager_folder_iam_member" "coi-vpc-admin-permissions" {
-  folder_id = var.yc_folder_id
-  role      = "vpc.admin"
-  member    = "serviceAccount:${yandex_iam_service_account.coi-sa.id}"
-}
-
-resource "yandex_resourcemanager_folder_iam_member" "coi-load-balancer-admin-permissions" {
-  folder_id = var.yc_folder_id
-  role      = "load-balancer.admin"
-  member    = "serviceAccount:${yandex_iam_service_account.coi-sa.id}"
-}
-
-resource "yandex_resourcemanager_folder_iam_member" "coi-iam-serviceAccounts-user-permissions" {
-  folder_id = var.yc_folder_id
-  role      = "iam.serviceAccounts.user"
-  member    = "serviceAccount:${yandex_iam_service_account.coi-sa.id}"
-}
-
 resource "time_sleep" "wait_for_coi_sa" {
   create_duration = "10s"
   depends_on      = [yandex_iam_service_account.coi-sa]
