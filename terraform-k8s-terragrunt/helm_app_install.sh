ed2af7250d9e24d6ccf26afbf582dc96837e2f6f
Anton Patsev <patsev.anton@gmail.com>
2023-02-14 20:13:05 +0600

    terraform-k8s-terragrunt helmfile apply -f helmfile-loki-distributed.yaml

--- terraform-k8s-terragrunt/helm_app_install.sh
+++ terraform-k8s-terragrunt/helm_app_install.sh
@@ -26,12 +26,12 @@ echo "================ Install kube-prometheus-stack ======================"
 helmfile apply -f helmfile-kube-prometheus-stack.yaml
 echo sleep 5
 sleep 5
-#echo "================ Install loki-distributed ======================"
-#helmfile apply -f helmfile-loki-distributed.yaml
-#echo sleep 5
-#sleep 5
-#echo "================ Install promtail ======================"
-#helmfile apply -f helmfile-promtail.yaml
+echo "================ Install loki-distributed ======================"
+helmfile apply -f helmfile-loki-distributed.yaml
+echo sleep 5
+sleep 5
+echo "================ Install promtail ======================"
+helmfile apply -f helmfile-promtail.yaml
 #echo sleep 5
 #sleep 5
 #echo "================ Install promtail ======================"
