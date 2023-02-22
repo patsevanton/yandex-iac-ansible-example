0a14de14abdd60b8dbfbcfab6252ed17ed096d19
Anton Patsev <patsev.anton@gmail.com>
2023-02-22 10:18:38 +0600

    prometheus-loki-2-7-loggenerator-in-docker change source terraform-yandex-compute

--- prometheus-loki-2-7-loggenerator-in-docker/provider.tf
+++ prometheus-loki-2-7-loggenerator-in-docker/provider.tf
@@ -2,7 +2,7 @@ terraform {
   required_providers {
     yandex = {
       source  = "yandex-cloud/yandex"
-      version = "0.84.0"
+      version = "0.85.0"
     }
     template = {
       source  = "terraform-registry.storage.yandexcloud.net/hashicorp/template"
