0202ff76ae49d347f6771792e1eef8d98fcb136a
Anton Patsev <patsev.anton@gmail.com>
2023-02-22 09:42:31 +0600

    terraform-k8s-terragrunt http_basic_auth_password_file

--- terraform-k8s-terragrunt/helm_app_remove.sh
+++ terraform-k8s-terragrunt/helm_app_remove.sh
@@ -8,11 +8,12 @@ export external_ipv4_address
 echo "$external_ipv4_address"
 cd ..
 
-helmfile destroy -f helmfile-loggenerator.yaml
-helmfile destroy -f helmfile-promtail.yaml
-helmfile destroy -f helmfile-loki-distributed.yaml
-helmfile destroy -f helmfile-ingress-nginx.yaml
+#helmfile destroy -f helmfile-loggenerator.yaml
+#helmfile destroy -f helmfile-promtail.yaml
+#helmfile destroy -f helmfile-loki-distributed.yaml
+#helmfile destroy -f helmfile-ingress-nginx.yaml
+helmfile destroy -f helmfile-prometheus-blackbox-exporter.yaml
 helmfile destroy -f helmfile-kube-prometheus-stack.yaml
 kubectl delete namespace monitoring || true
-kubectl delete namespace ingress-nginx|| true
+#kubectl delete namespace ingress-nginx|| true
 rm -f "/home/$USER/.kube/config"
