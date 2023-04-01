4f833bf0eec093580fd925e049de63ef0618e271
Anton Patsev <patsev.anton@gmail.com>
2023-04-01 11:56:01 +0600

    terraform-k8s-alertmanager-loki add Loki, promtail

--- terraform-k8s-alertmanager-loki/helm_app_install.sh
+++ terraform-k8s-alertmanager-loki/helm_app_install.sh
@@ -12,12 +12,40 @@ helm repo add prometheus-community https://prometheus-community.github.io/helm-c
 kubectl create namespace prometheus || true
 helm upgrade --install --wait -n prometheus kube-prometheus-stack prometheus-community/kube-prometheus-stack -f values-kube-prometheus-stack.yaml
 
-#echo ""
-#echo "Install redis"
-#helm repo add archive-full-bitnami https://raw.githubusercontent.com/bitnami/charts/archive-full-index/bitnami
-##helm repo update
-#kubectl create namespace redis || true
-#helm upgrade --install --wait -n redis redis archive-full-bitnami/redis --version 17.4.2 -f values-redis.yaml
+
+echo "http://loki-loki-distributed-gateway.loki"
+echo ""
+echo "Install Microservices deployment Loki"
+helm repo add grafana https://grafana.github.io/helm-charts
+helm repo update
+kubectl create namespace loki || true
+access_key_id=$(terraform output --raw yandex_storage_bucket_loki_access_key)
+export access_key_id
+secret_access_key=$(terraform output --raw yandex_storage_bucket_loki_secret_key)
+export secret_access_key
+bucket=$(terraform output --raw yandex_storage_bucket_loki_bucket)
+export bucket
+helm upgrade --install --wait loki grafana/loki-distributed -n loki \
+    --set "loki.storageConfig.aws.access_key_id=$access_key_id"  \
+    --set "loki.storageConfig.aws.secret_access_key=$secret_access_key"  \
+    --set "loki.storageConfig.aws.bucketnames=$bucket"  \
+    -f value-loki-distributed.yaml  --debug --version 0.69.9
+
+
+echo ""
+echo "Install Promtail"
+helm repo add grafana https://grafana.github.io/helm-charts
+helm repo update
+kubectl create namespace promtail || true
+helm upgrade --install --wait promtail grafana/promtail -n promtail --set "loki.serviceName=loki" --version 6.9.3 -f values-promtail.yaml  --debug
+
+
+echo ""
+echo "Install loggenerator"
+kubectl create namespace loggenerator || true
+helm upgrade --install --wait loggenerator -n loggenerator ./loggenerator --set replicaCount=1  --debug
+
+
 
 end_time=$(date +%s)
 date2=$(date +"%s")
