1774908b498f98021205c9d4b383b18f2a827eee
Anton Patsev <patsev.anton@gmail.com>
2023-04-14 17:07:08 +0600

    terraform-k8s-istio-prometheus-stack

---
+++ terraform-k8s-istio-prometheus-stack/helm_app_install.sh
@@ -0,0 +1,41 @@
+#!/bin/bash
+
+set -eu pipefail
+
+start_time=$(date +%s)
+date1=$(date +"%s")
+
+helm repo add istio https://istio-release.storage.googleapis.com/charts
+# helm repo update
+kubectl create namespace istio-system  || true
+helm install istio-base istio/base -n istio-system --version 1.17.2
+helm install istiod istio/istiod -n istio-system --version 1.17.2
+export Promgrafana_LoadBalancerIP=$(terraform output --raw Promgrafana_LoadBalancerIP)
+helm install istio-ingressgateway -n istio-system istio/gateway --version 1.17.2 --set service.loadBalancerIP="$Promgrafana_LoadBalancerIP"
+
+helm install -n istio-system --set auth.strategy="anonymous" \
+  --repo https://kiali.org/helm-charts \
+  kiali-server kiali-server
+
+export Promgrafana_LoadBalancerIP=$(terraform output --raw Promgrafana_LoadBalancerIP)
+#echo "Promgrafana_LoadBalancerIP=$Promgrafana_LoadBalancerIP"
+#helm upgrade --install --wait ingress-nginx ingress-nginx/ingress-nginx \
+#    --set controller.service.loadBalancerIP="$Promgrafana_LoadBalancerIP" \
+#    --version 4.6.0 # -f value-ingress-nginx.yaml
+
+
+#echo ""
+#echo "Install Kube-Prometheus-Stack"
+#helm repo add prometheus-community https://prometheus-community.github.io/helm-charts
+##helm repo update
+#kubectl create namespace prometheus || true
+#helm upgrade --install --wait -n prometheus kube-prometheus-stack prometheus-community/kube-prometheus-stack -f values-kube-prometheus-stack.yaml
+#
+
+end_time=$(date +%s)
+date2=$(date +"%s")
+echo "###############"
+echo "Execution time was $(( end_time - start_time )) s."
+DIFF=$(( date2 - date1 ))
+echo "Duration: $(( DIFF / 3600 )) hours $((( DIFF % 3600) / 60 )) minutes $(( DIFF % 60 )) seconds"
+echo "###############"
---
+++ terraform-k8s-istio-prometheus-stack/helm_release.tf
@@ -0,0 +1,39 @@
+#provider "helm" {
+#  kubernetes {
+#    host                   = yandex_kubernetes_cluster.loki_k8s_cluster.master[0].external_v4_endpoint
+#    cluster_ca_certificate = yandex_kubernetes_cluster.loki_k8s_cluster.master[0].cluster_ca_certificate
+#    exec {
+#      api_version = "client.authentication.k8s.io/v1beta1"
+#      args        = ["k8s", "create-token"]
+#      command     = "yc"
+#    }
+#  }
+#}
+#
+#resource "helm_release" "ingress_nginx" {
+#  name       = "ingress-nginx"
+#  repository = "https://kubernetes.github.io/ingress-nginx"
+#  chart      = "ingress-nginx"
+#  version    = "4.6.0"
+#  wait       = true
+#  depends_on = [
+#    yandex_kubernetes_node_group.loki-k8s-node-group
+#  ]
+#
+#  set {
+#    name  = "controller.service.loadBalancerIP"
+#    value = yandex_vpc_address.promgrafana_address.external_ipv4_address[0].address
+#  }
+#
+#  set {
+#    name  = "controller.config.log-format-escape-json"
+#    value = true
+#  }
+#
+#  set {
+#    name  = "controller.config.log-format-upstream"
+#    value = "{\"time\": \"$time_iso8601\", \"remote_addr\": \"$proxy_protocol_addr\"}"
+#  }
+#
+#}
+#
