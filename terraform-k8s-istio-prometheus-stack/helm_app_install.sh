9c66bdba552c82a35f3d1247556ad34fae3377f0
Anton Patsev <patsev.anton@gmail.com>
2023-04-14 17:15:37 +0600

    terraform-k8s-istio-prometheus-stack upgrade --install --wait

--- terraform-k8s-istio-prometheus-stack/helm_app_install.sh
+++ terraform-k8s-istio-prometheus-stack/helm_app_install.sh
@@ -8,12 +8,12 @@ date1=$(date +"%s")
 helm repo add istio https://istio-release.storage.googleapis.com/charts
 # helm repo update
 kubectl create namespace istio-system  || true
-helm install istio-base istio/base -n istio-system --version 1.17.2
-helm install istiod istio/istiod -n istio-system --version 1.17.2
+helm upgrade --install --wait istio-base istio/base -n istio-system --version 1.17.2
+helm upgrade --install --wait istiod istio/istiod -n istio-system --version 1.17.2
 export Promgrafana_LoadBalancerIP=$(terraform output --raw Promgrafana_LoadBalancerIP)
-helm install istio-ingressgateway -n istio-system istio/gateway --version 1.17.2 --set service.loadBalancerIP="$Promgrafana_LoadBalancerIP"
+helm upgrade --install --wait istio-ingressgateway -n istio-system istio/gateway --version 1.17.2 --set service.loadBalancerIP="$Promgrafana_LoadBalancerIP"
 
-helm install -n istio-system --set auth.strategy="anonymous" \
+helm upgrade --install --wait -n istio-system --set auth.strategy="anonymous" \
   --repo https://kiali.org/helm-charts \
   kiali-server kiali-server
 
