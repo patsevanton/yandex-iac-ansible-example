73d78de24077ac088e775c070c0152615e137eb6
Anton Patsev <patsev.anton@gmail.com>
2023-05-20 21:44:26 +0600

    terraform-ydb-database-serverless

---
+++ terraform-ydb-database-serverless/main.tf
@@ -0,0 +1,27 @@
+resource "yandex_ydb_database_serverless" "database1" {
+  name = "test-ydb-serverless"
+
+  serverless_database {
+    enable_throttling_rcu_limit = false
+    provisioned_rcu_limit       = 10
+    storage_size_limit          = 50
+    throttling_rcu_limit        = 0
+  }
+}
+
+
+data "yandex_vpc_network" "default" {
+  name = "default"
+}
+
+data "yandex_vpc_subnet" "default-ru-central1-a" {
+  name = "default-ru-central1-a"
+}
+
+data "yandex_vpc_subnet" "default-ru-central1-b" {
+  name = "default-ru-central1-b"
+}
+
+data "yandex_vpc_subnet" "default-ru-central1-c" {
+  name = "default-ru-central1-c"
+}
