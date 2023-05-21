a91599ed7c40441fb827a1059d15c0d16dddb1c8
Anton Patsev <patsev.anton@gmail.com>
2023-05-21 20:14:18 +0600

    version = "0.91.0"

--- vault_docker/provider.tf
+++ vault_docker/provider.tf
@@ -2,7 +2,7 @@ terraform {
   required_providers {
     yandex = {
       source  = "yandex-cloud/yandex"
-      version = "0.88.0"
+      version = "0.91.0"
     }
     template = {
       source  = "terraform-registry.storage.yandexcloud.net/hashicorp/template"
