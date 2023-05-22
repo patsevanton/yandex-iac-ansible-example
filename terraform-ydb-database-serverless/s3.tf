34c9ab908a00dad7ec5537cc899cb5b98ac7a324
Anton Patsev <patsev.anton@gmail.com>
2023-05-22 15:14:30 +0600

    Disable create yandex_storage_bucket

--- terraform-ydb-database-serverless/s3.tf
+++ terraform-ydb-database-serverless/s3.tf
@@ -5,12 +5,12 @@ resource "yandex_iam_service_account_static_access_key" "ydb-sa-static-key" {
 }
 
 ## Use keys to create bucket
-resource "yandex_storage_bucket" "ydb" {
-  access_key = yandex_iam_service_account_static_access_key.ydb-sa-static-key.access_key
-  secret_key = yandex_iam_service_account_static_access_key.ydb-sa-static-key.secret_key
-  bucket     = "ydb-backet-anton-patsev"
-  force_destroy = true
-}
+#resource "yandex_storage_bucket" "ydb" {
+#  access_key = yandex_iam_service_account_static_access_key.ydb-sa-static-key.access_key
+#  secret_key = yandex_iam_service_account_static_access_key.ydb-sa-static-key.secret_key
+#  bucket     = "ydb-bucket-anton-patsev"
+#  force_destroy = true
+#}
 
 resource "local_file" "credentials" {
   content = templatefile("credentials.tftpl",
@@ -19,26 +19,23 @@ resource "local_file" "credentials" {
       secret_key = yandex_iam_service_account_static_access_key.ydb-sa-static-key.secret_key
     }
   )
-  depends_on = [
-    yandex_storage_bucket.ydb
-  ]
   filename = "credentials"
 }
 
 output "yandex_storage_bucket_ydb_access_key" {
   description = "access_key yandex_storage_bucket of ydb"
-  value       = yandex_storage_bucket.ydb.access_key
+  value       = yandex_iam_service_account_static_access_key.ydb-sa-static-key.access_key
   sensitive   = true
 }
 
 output "yandex_storage_bucket_ydb_secret_key" {
   description = "secret_key yandex_storage_bucket of ydb"
-  value       = yandex_storage_bucket.ydb.secret_key
+  value       = yandex_iam_service_account_static_access_key.ydb-sa-static-key.secret_key
   sensitive   = true
 }
 
-output "yandex_storage_bucket_ydb_bucket" {
-  description = "name bucket of ydb"
-  value       = yandex_storage_bucket.ydb.bucket
-  sensitive   = true
-}
+#output "yandex_storage_bucket_ydb_bucket" {
+#  description = "name bucket of ydb"
+#  value       = yandex_storage_bucket.ydb.bucket
+#  sensitive   = true
+#}
--- terraform-ydb-database-serverless/sa-storage-admin.tf
+++ terraform-ydb-database-serverless/sa-storage-admin.tf
@@ -1,18 +1,18 @@
 ## Create SA sa-storage-admin
 resource "yandex_iam_service_account" "ydb-sa" {
   folder_id = var.yc_folder_id
-  name      = "sa-storage-admin"
+  name      = "sa-ydb-admin"
 }
 
 ## Grant permissions
-resource "yandex_resourcemanager_folder_iam_member" "ydb-sa-storage-admin" {
+resource "yandex_resourcemanager_folder_iam_member" "ydb-sa-ydb-admin" {
   folder_id = var.yc_folder_id
-  role      = "storage.admin"
+  role      = "ydb.admin"
   member    = "serviceAccount:${yandex_iam_service_account.ydb-sa.id}"
 }
 
-resource "yandex_resourcemanager_folder_iam_member" "ydb-sa-compute-admin" {
-  folder_id = var.yc_folder_id
-  role      = "compute.admin"
-  member    = "serviceAccount:${yandex_iam_service_account.ydb-sa.id}"
-}
+#resource "yandex_resourcemanager_folder_iam_member" "ydb-sa-compute-admin" {
+#  folder_id = var.yc_folder_id
+#  role      = "compute.admin"
+#  member    = "serviceAccount:${yandex_iam_service_account.ydb-sa.id}"
+#}
