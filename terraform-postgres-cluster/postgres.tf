f364f5e5cc06897bcc39f935989acc8f2a503057
Anton Patsev <patsev.anton@gmail.com>
2023-05-24 18:07:53 +0600

    terraform-postgres-cluster

---
+++ terraform-postgres-cluster/postgres.tf
@@ -0,0 +1,53 @@
+resource "yandex_mdb_postgresql_cluster" "test" {
+  name                = "test"
+  environment         = "PRODUCTION"
+  network_id          = data.yandex_vpc_network.default.id
+
+  config {
+    version = "15"
+    resources {
+      resource_preset_id = "c3-c2-m4"
+      disk_type_id       = "network-ssd"
+      disk_size          = 20
+    }
+    performance_diagnostics {
+      enabled = true
+      sessions_sampling_interval  = 60
+      statements_sampling_interval = 600
+    }
+  }
+
+  host {
+    name      = "test"
+    zone      = data.yandex_vpc_subnet.default-ru-central1-a.zone
+    subnet_id = data.yandex_vpc_subnet.default-ru-central1-a.id
+    assign_public_ip = true
+  }
+  host {
+    name      = "test"
+    zone      = data.yandex_vpc_subnet.default-ru-central1-a.zone
+    subnet_id = data.yandex_vpc_subnet.default-ru-central1-a.id
+    assign_public_ip = true
+  }
+  host {
+    name      = "test"
+    zone      = data.yandex_vpc_subnet.default-ru-central1-a.zone
+    subnet_id = data.yandex_vpc_subnet.default-ru-central1-a.id
+    assign_public_ip = true
+  }
+}
+
+resource "yandex_mdb_postgresql_database" "test" {
+  cluster_id = yandex_mdb_postgresql_cluster.test.id
+  name       = "test"
+  owner      = "test"
+  depends_on = [
+    yandex_mdb_postgresql_user.test
+  ]
+}
+
+resource "yandex_mdb_postgresql_user" "test" {
+  cluster_id = yandex_mdb_postgresql_cluster.test.id
+  name       = "test"
+  password   = "dsgvdgsbsdznbzdfsndf"
+}
