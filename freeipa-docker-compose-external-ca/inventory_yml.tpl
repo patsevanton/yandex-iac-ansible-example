006571b7c3de20efbe76df680647a81e832a73ee
Anton Patsev <patsev.anton@gmail.com>
2024-02-17 11:13:54 +0600

    freeipa_internal_ip

--- freeipa-docker-compose-external-ca/inventory_yml.tpl
+++ freeipa-docker-compose-external-ca/inventory_yml.tpl
@@ -8,6 +8,6 @@ all:
     ansible_user:  ${ssh_user}
     ansible_ssh_private_key_file: ~/.ssh/id_rsa
     freeipa_password: "${freeipa_password}"
-    freeipa_fqdn: "${freeipa_fqdn}"
+    freeipa_fqdn: "${freeipa_internal_ip}.${freeipa_domain}"
     freeipa_domain: "${freeipa_domain}"
     ssh_user: "${ssh_user}"
--- freeipa-docker-compose-external-ca/main.tf
+++ freeipa-docker-compose-external-ca/main.tf
@@ -16,18 +16,24 @@ module "freeipa" {
 resource "local_file" "inventory_yml" {
   content = templatefile("inventory_yml.tpl",
     {
-      ssh_user           = var.ssh_user
-      freeipa_public_ip  = module.freeipa.external_ip[0]
-      freeipa_password   = var.freeipa_password
-      freeipa_fqdn       = var.freeipa_fqdn
-      freeipa_domain     = var.freeipa_domain
-      ssh_user           = var.ssh_user
+      ssh_user             = var.ssh_user
+      freeipa_public_ip    = module.freeipa.external_ip[0]
+      freeipa_internal_ip  = module.freeipa.internal_ip[0]
+      freeipa_password     = var.freeipa_password
+      freeipa_fqdn         = var.freeipa_fqdn
+      freeipa_domain       = var.freeipa_domain
+      ssh_user             = var.ssh_user
     }
   )
   filename = "inventory.yml"
 }
 
 output "freeipa_public_ip" {
-  description = "Public IP address for active directory"
+  description = "Public IP address FreeIPA"
   value       = module.freeipa.external_ip[0]
+}
+
+output "freeipa_internal_ip" {
+  description = "Internal IP address FreeIPA"
+  value       = module.freeipa.internal_ip[0]
 }
